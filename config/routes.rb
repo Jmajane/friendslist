c60d3885d30604342cc65a1c40b2944cb629d00f
Jmajane <jordan.majane@outlook.com>
2022-09-21 12:30:45 -0400

    hidding friends from users who did not create them

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :friends
-  root 'friends#index'
   get 'home/about'
+  root 'friends#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
